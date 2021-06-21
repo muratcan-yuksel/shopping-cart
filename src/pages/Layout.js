6626218b4b43825950f1713d85a5f1290de2727d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-06-21 08:00:06 +0300

    added the children thing into layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from "./Navbar"
 
-export default function Layout() {
+export default function Layout({children}) {
     return (
         <div className="layout">
             <Navbar />
             <div className="content">
-                {/* content goes here */}
+                {/* in order for the content outside of the layour to be seen
+                for instance the content in index.js
+                I need to do this "children" thing */}
+                {children}
             </div>
             <footer>
                 <p>Copyright 2021 Murat Can Yüksel</p>
