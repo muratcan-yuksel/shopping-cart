b7525b3ff5276945aec35c20af42cff6de326101
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-06-22 18:33:48 +0300

    managed to scale the welcome image, but footer needs attention

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Layout from "./Layout"
+import {indexContainer} from "../styles/layout.module.css"
 
 export default function Home() {
   return (
   <Layout>
-<div>Hello world bruh</div>
+<div className={indexContainer}>
+{/* <div>Hello world bruh</div> */}
+
+<img src="/welcome.jpg" style={{maxWidth:"100%"}, {maxHeight:"60vh"}} />
+</div>
   </Layout>
   )
 }
