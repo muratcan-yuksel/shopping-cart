b117b311076acec096917e0ef37e1d755360ef42
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-06-22 21:22:16 +0300

    fallback

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -12,10 +12,26 @@ import Freedom from "../images/radical-freedom.png"
 import Revolution from "../images/revolution.png"
 import UpsideHegel from "../images/upside-hegel.png"
 
+const Cards= (props)=>[
+{title:"Anarchism T-shirt", price:20, id:uniqid(), img:Anarchism, sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"},
+{title:"", price:"", id:"", img:"", sign:"$"}
+]
+
+
+
 export default function Shop() {
     return (
         <Layout>
         <div>
+            <p>Cards[0].price</p>
 
         </div>
         </Layout>
